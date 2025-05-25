6fc21bd9d53f71500b8a395d6fb1a9816e3e68e7
basma elkhalfi <basmaelkhalfi56@gmail.com>
2025-05-25 01:57:46 +0100

    Mise à jour du message dans index.js

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Bonjour, ceci est une application simple sur Azure !');
-});
+res.send('Bonjour, ceci est une application mise à jour sur Azure !');});
 
 app.listen(port, () => {
     console.log(`Serveur en cours d'exécution sur le port ${port}`);
